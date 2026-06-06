95f001207813472f6598707974f8d51b86076e4f
Ameen010 <ameenkido5412@gmail.com>
2026-06-06 18:56:38 +0530

    fix sitemap and robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,14 +1,11 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-  // Replace with your actual domain name if it is different
-  const baseUrl = 'https://ameen-portfolio.pages.dev';
-
   return {
     rules: {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: `${baseUrl}/sitemap.xml`,
+    sitemap: 'https://ameen-portfolio.pages.dev/sitemap.xml',
   };
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,12 +1,9 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  // Replace with your actual domain name if it is different
-  const baseUrl = 'https://ameennoushad.com';
-
   return [
     {
-      url: baseUrl,
+      url: 'https://ameen-portfolio.pages.dev',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
